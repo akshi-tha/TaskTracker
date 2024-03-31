8dabe96e595d609563df7fcdaac0657fdefbcf96
Akshitha V <126058412+akshi-tha@users.noreply.github.com>
2024-03-31 21:37:42 +0530

    Update Header.js

--- src/mycomponents/Header.js
+++ src/mycomponents/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a> {/* Corrected closing tag */}
+        <a className="navbar-brand" href="#">{props.title}</a> {/* Corrected closing tag */}
           
         <button
           className="navbar-toggler"
@@ -21,7 +21,7 @@ export default function Header(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
